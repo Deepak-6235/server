6f696055584f23340793e807eddad538f730ef14
Deepak-6235 <deepak.vk@semikolen.com>
2026-01-30 11:12:57 +0530

    .

--- app/core/config.py
+++ app/core/config.py
@@ -7,3 +7,9 @@ FRONTEND_URL = os.getenv("FRONTEND_URL", "http://localhost:5173")
 BASE_URL = os.getenv("BASE_URL", "http://127.0.0.1:8000")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 # OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+# AWS S3 Configuration
+S3_ACCESS_KEY = os.getenv("S3_ACCESS_KEY")
+S3_SECRET_KEY = os.getenv("S3_SECRET_KEY")
+S3_BUCKET = os.getenv("S3_BUCKET")
+S3_REGION = os.getenv("S3_REGION", "ap-south-1")
